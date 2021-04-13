644e0e25d88264585e54a74250062dea793eacdc
EmilLM <luchianemil@gmail.com>
2021-04-13 21:43:23 +0300

    places api

--- functions/geocode/index.js
+++ functions/geocode/index.js
@@ -1,8 +1,22 @@
 const { locations: locationsMock } = require('./geocode.mock');
+const functions = require('firebase-functions');
 
 module.exports.geocodeRequest = (request, response, client) => {
-	const { city } = request.query;
-	const locationMock = locationsMock[city.toLowerCase()];
-	console.log('cityParam', city);
-	response.json(locationMock);
+	const { city, mock } = request.query;
+	if (mock === 'true') {
+		const locationMock = locationsMock[city.toLowerCase()];
+		return response.json(locationMock);
+	}
+	client.geocode({
+		params: {
+			address: city,
+			key: functions.config().google.key
+		},
+		timeout: 1000,
+	}).then(res=>{
+		return response.json(res.data)
+	}).catch(e=>{
+		response.status(400);
+		return response.send(e.response.data.error.message)
+	})
 };
--- functions/places/index.js
+++ functions/places/index.js
@@ -1,10 +1,46 @@
 const { mocks, addMockImage } = require('./mock');
+const functions = require('firebase-functions');
 
+const addGoogleImage = (restaurant) => {
+	const ref = restaurant.photos[0].photo_reference;
+	if (!ref) {
+		restaurant.photos = [
+			'https://www.foodiesfeed.com/wp-content/uploads/2019/06/top-view-for-box-of-2-burgers-home-made-600x899.jpg',
+		];
+		return restaurant;
+	}
+	restaurant.photos = [
+		`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${ref}&key=${
+		  functions.config().google.key
+		}`,
+	  ];
+	return restaurant;
+};
 
-module.exports.placesRequest = (request, response) => {
-	const { location } = request.query
-	console.log('locParam', location)
-	const data = mocks[location];
-    if (data) data.results = data.results.map(addMockImage);
-	response.json(data)
+module.exports.placesRequest = (request, response, client) => {
+	const { location, mock } = request.query;
+	if (mock === 'true') {
+		const data = mocks[location];
+		if (data) data.results = data.results.map(addMockImage);
+		response.json(data);
+	}
+	client
+		.placesNearby({
+			params: {
+				location,
+				radius: 1500,
+				type: 'restaurant',
+				key: functions.config().google.key,
+			},
+			timeout: 1000,
+		})
+		.then((res) => {
+			re.data.results = res.data.results.map(addGoogleImage);
+
+			return response.json(res.data);
+		})
+		.catch((e) => {
+			response.status(400);
+			return response.send(e.response.data.error.message);
+		});
 };
